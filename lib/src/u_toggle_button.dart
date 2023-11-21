f62dc1c24e4087d1215a83856fd270493bdea491
LoneChen47 <986759095@qq.com>
2023-11-21 23:12:40 +0800

    first commit

---
+++ lib/src/u_toggle_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UToggleButton extends StatefulWidget {
+  const UToggleButton({super.key});
+
+  @override
+  State<UToggleButton> createState() => _UToggleButtonState();
+}
+
+class _UToggleButtonState extends State<UToggleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
